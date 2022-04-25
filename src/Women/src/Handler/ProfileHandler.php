5a95dedbc6bf870fe25ed42f15920c3c813e57e9
Catherine Anicehebe <anichebecatherine#gmail.com>
2022-04-25 19:40:24 +0100

    profile

--- src/Women/src/Handler/ProfileHandler.php
+++ src/Women/src/Handler/ProfileHandler.php
@@ -8,10 +8,18 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Laminas\Diactoros\Response\HtmlResponse;
+use Laminas\Diactoros\Response\JsonResponse;
+use Doctrine\ORM\EntityManager;
+use Women\Entity\Profile;
 use Mezzio\Template\TemplateRendererInterface;
-
+use Authentication\Entity\User;
 class ProfileHandler implements RequestHandlerInterface
 {
+        /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
     /**
      * @var TemplateRendererInterface
      */
@@ -20,15 +28,34 @@ class ProfileHandler implements RequestHandlerInterface
     public function __construct(TemplateRendererInterface $renderer)
     {
         $this->renderer = $renderer;
+        $this->entityManager = '$em';
     }
 
     public function handle(ServerRequestInterface $request) : ResponseInterface
     {
-        // Do some work...
-        // Render and return a response:
-        return new HtmlResponse($this->renderer->render(
-            'women::profile',
-            [] // parameters to pass to template
-        ));
+        $em = $this->entityManager;
+        try {
+            if ("POST" === $request->getMethod()) {
+                $post = $request->getParsedBody();
+                $profileEntity = new Profile();
+                $dob = strip_tags($post["profile_dob"]);
+                $name = strip_tags($post["profile_name"]);
+                $profileEntity
+                ->setProfileDob($dob)
+                ->setCreatedAt(new DateTime("now"))
+                ->setProfileName($name)
+
+            $em->persist($profileEntity);
+            $em->flush();
+        }
+    } catch (\Throwable $th) {
+        return new JsonResponse([
+            "error" => $th->getMessage()
+        ]);
+    }
+
+    return new JsonResponse([
+        "status" => "success"
+        ], 201);
     }
 }
